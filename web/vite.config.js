97abb94050f5ce8673a0723ced07e61f69c05a50
Manish <manishsingh10895@gmail.com>
2023-09-03 04:57:38 +0530

    pre branch change

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,8 +1,8 @@
-import {defineConfig} from 'vite'
-import wasm from 'vite-plugin-wasm'
-import {svelte} from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import wasm from "vite-plugin-wasm";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [wasm(), svelte()]
-})
+  plugins: [wasm(), svelte()],
+});
